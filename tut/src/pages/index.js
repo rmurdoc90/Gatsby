fa0a5ab2075dab645659905f5bc00c6f9a96781f
rmurdoc90 <rmurdoc90@gmail.com>
2022-01-04 22:26:42 -0800

    pages created

--- tut/src/pages/index.js
+++ tut/src/pages/index.js
@@ -1,11 +1,9 @@
-import * as React from "react"
+import React from 'react'
 
-function index() {
+export default function index() {
   return (
     <div>
-      <h1>Hello World!!!!</h1>
+      <h1>home page</h1>
     </div>
   )
 }
-
-export default index
