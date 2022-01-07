3f59fbac5282f21d7e76664c7661182a9e0207e5
rmurdoc90 <rmurdoc90@gmail.com>
2022-01-06 16:07:12 -0800

    contact content added

--- tut/src/pages/contact.js
+++ tut/src/pages/contact.js
@@ -4,7 +4,42 @@ import Layout from "../components/Layout"
 export default function Contact() {
   return (
     <Layout>
-      <h1>Contact Page</h1>
+      <main className="page">
+        <section classname="contact-page">
+          <article className="contact-info">
+            <h3>Want To Get In Touch?</h3>
+            <p>
+              Squid yr offal keffiyeh, cold-pressed occupy kickstarter. Kale
+              chips tousled butcher taiyaki roof party glossier marfa paleo
+              crucifix.
+            </p>
+            <p>
+              Williamsburg four loko vape skateboard palo santo, tumblr raclette
+              leggings. Heirloom vegan plaid kombucha jean shorts.
+            </p>
+            <p>Street art pork belly stumptown squid synth gluten-free.</p>
+          </article>
+          <article>
+            <form className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">your name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">your email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">your message</label>
+                <textarea name="message" id="message"></textarea>
+              </div>
+              <button type="submit" className="btn block">
+                submit
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   )
 }
