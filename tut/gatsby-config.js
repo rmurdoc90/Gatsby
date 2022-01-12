7915a2a9b2bb8bd092ebe9b8acd62029dd09afed
Rob <rob@Janieces-MacBook-Pro.local>
2022-01-11 16:38:00 -0800

    adding more stuff

--- tut/gatsby-config.js
+++ tut/gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `styles`,
+        path: `${__dirname}/src/assets/css`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
