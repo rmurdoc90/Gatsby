6032c20efe764b6069760dbb500c8d0b8d12921f
Rob <rob@Janieces-MacBook-Pro.local>
2022-01-14 08:43:59 -0800

    adding data

--- tut/src/components/AllRecipes.js
+++ tut/src/components/AllRecipes.js
@@ -1,8 +1,31 @@
 import React from "react"
 import TagsList from "./TagsList"
 import RecipesList from "./RecipesList"
+import { useStaticQuery, graphql} from 'gatsby'
+
+const query = graphql`
+  {
+    allContentfulRecipes(sort: {fields: title, order: ASC}) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        content {
+          tags
+        }
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
 
 const AllRecipes = () => {
+  const data  = useStaticQuery(query)
+  const recipes = data.allContentfulRecipes.nodes
+  console.log(recipes)
   return (
     <div>
       all recipes
--- tut/src/pages/recipes.js
+++ tut/src/pages/recipes.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/Layout"
 import AllRecipes from "../components/AllRecipes"
 
+
 export default function Recipes() {
   return (
     <Layout>
