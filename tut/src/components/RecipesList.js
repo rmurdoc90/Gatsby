ea86b64ecd2bd1e80fa4b9706a78dda59040ebae
Robert Murdoch <rmurdoc90@gmail.com>
2022-01-14 20:25:11 -0800

    adding links to recipes

--- tut/src/components/RecipesList.js
+++ tut/src/components/RecipesList.js
@@ -1,7 +1,28 @@
 import React from "react"
+import { Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
-const RecipesList = () => {
-  return <div>recipes list</div>
+const RecipesList = ({ recipes = [] }) => {
+  return (
+    <div className="recipes-list">
+      {recipes.map(recipes => {
+        const { id, title, image, prepTime, cookTime } = recipes
+        return (
+          <Link key={id} to={`/${title}`} className="recipe">
+            <GatsbyImage
+              image={image.gatsbyImageData}
+              className="recipe-img"
+              alt={title}
+            />
+            <h5>{title}</h5>
+            <p>
+              Prep : {prepTime}min | cook : {cookTime}
+            </p>
+          </Link>
+        )
+      })}
+    </div>
+  )
 }
 
 export default RecipesList
--- tut/src/pages/recipes.js
+++ tut/src/pages/recipes.js
@@ -2,7 +2,6 @@ import React from "react"
 import Layout from "../components/Layout"
 import AllRecipes from "../components/AllRecipes"
 
-
 export default function Recipes() {
   return (
     <Layout>
