4fb496b1efe79c709341b2e3ef2275053f63de63
rmurdoc90 <rmurdoc90@gmail.com>
2022-01-21 12:06:52 -0800

    testing

--- tut/src/pages/404.js
+++ tut/src/pages/404.js
@@ -8,6 +8,7 @@ export default function Error() {
         <section>
           <h1>404</h1>
           <h3>page not found</h3>
+          <h4>Testing</h4>
         </section>
       </main>
     </Layout>
