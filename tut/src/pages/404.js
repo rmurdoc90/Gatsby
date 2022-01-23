96e6ecd5be714539f34f2c88a02973fbb026374b
rmurdoc90 <rmurdoc90@gmail.com>
2022-01-23 14:58:20 -0800

    preping for deployment

--- tut/src/pages/404.js
+++ tut/src/pages/404.js
@@ -3,15 +3,13 @@ import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
 export default function Error() {
-
   return (
     <Layout>
-      <SEO title='404 Page'/>
+      <SEO title="404 Page" />
       <main className="error-page">
         <section>
           <h1>404</h1>
           <h3>page not found</h3>
-          <h4>Testing</h4>
         </section>
       </main>
     </Layout>
